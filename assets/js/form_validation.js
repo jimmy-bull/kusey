b724cec79f608ad9b429e83898bdf42525be5a0b
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-04-02 20:17:20 +0200

    Update form_validation.js

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,33 +1,27 @@
 
 $('.error_txt').hide()
 let email_validator_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-let number_verify = 0;
+
 $('.btn_submit_contact').click(function (e) {
     if (email_validator_regex.test($('#email_txt_contact').val())) {
         $('#email_txt_contact').siblings().hide();
-        number_verify = 1;
     } else {
         $('#email_txt_contact').siblings().show();
-        number_verify = 0;
     }
 
     if ($('#name_txt_contact').val().trim() != "") {
         $('#name_txt_contact').siblings().hide();
-        number_verify = 1;
     } else {
         $('#name_txt_contact').siblings().show();
-        number_verify = 0;
     }
 
     if ($('#message_txt_contact').val().trim() != "") {
         $('#message_txt_contact').siblings().hide();
-        number_verify = 1;
     } else {
         $('#message_txt_contact').siblings().show();
-        number_verify = 0;
     }
 
-    if (number_verify == 1) {
+    if (email_validator_regex.test($('#email_txt_contact').val()) || $('#message_txt_contact').val().trim() != "" || $('#name_txt_contact').val().trim() != "") {
         Swal.fire({
             position: 'center',
             icon: 'success',
