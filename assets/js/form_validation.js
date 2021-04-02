97c9dee99e4feacb8eeecd2185c2162fc505263b
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-04-02 19:19:05 +0200

    Update form_validation.js

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -3,7 +3,7 @@ $('.error_txt').hide()
 let email_validator_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 $('.btn_submit_contact').click(function (e) {
 
-    if (email_validator_regex.test($('.email_txt_contact'))) {
+    if (email_validator_regex.test($('.email_txt_contact').val())) {
         alert('ok')
     } else {
         $('.email_txt_contact').siblings().show();
