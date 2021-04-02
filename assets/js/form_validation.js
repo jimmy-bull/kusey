6a466371c7a3e54149837cf04135160c32b56fcc
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-04-02 19:57:47 +0200

    Update form_validation.js

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,21 +1,25 @@
 
 $('.error_txt').hide()
 let email_validator_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+let number_verify = 0;
 $('.btn_submit_contact').click(function (e) {
     if (email_validator_regex.test($('#email_txt_contact').val())) {
-        alert('ok')
+        $('#email_txt_contact').siblings().hide();
+        number_verify = 1;
     } else {
         $('#email_txt_contact').siblings().show();
+        number_verify = 0;
     }
 
-    /*Swal.fire({  
-        position: 'center',
-        icon: 'success',
-        title: 'Your message has been sent successfully!',
-        showConfirmButton: false,
-        timer: 1500
-    })*/
-
+    if (number_verify == 1) {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Your message has been sent successfully!',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
     //name_txt_contact email_txt_contact message_txt_contact
 
 })
