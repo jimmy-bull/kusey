489bfdb7414b38d16717bac02dac20e1b5cb6e2c
jimmy-bull <47826838+jimmy-bull@users.noreply.github.com>
2021-04-02 19:18:15 +0200

    Update form_validation.js

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,12 +1,23 @@
 
 $('.error_txt').hide()
+let email_validator_regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 $('.btn_submit_contact').click(function (e) {
-    Swal.fire({
+
+    if (email_validator_regex.test($('.email_txt_contact'))) {
+        alert('ok')
+    } else {
+        $('.email_txt_contact').siblings().show();
+    }
+
+    /*Swal.fire({  
         position: 'center',
         icon: 'success',
         title: 'Your message has been sent successfully!',
         showConfirmButton: false,
         timer: 1500
-    })
+    })*/
+
+    //name_txt_contact email_txt_contact message_txt_contact
+
 })
 
